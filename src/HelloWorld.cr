69e9c97187bb469aacf52cd2767b376e4c814a17
Angel Tesillos <atesillos@luc.edu>
2022-09-26 13:14:08 -0500

    Added 'src' file. Added 'HelloWorld.cr" for a quick demonstration on some basic cr-l syntax.

---
+++ src/HelloWorld.cr
@@ -0,0 +1,38 @@
+# Angel Tesillos
+# Loyola University Chicago
+# 9/25/2022 
+# COMP 371: Programming languages
+# Crystal Programming Language`
+puts "Hello, world!"
+
+# Our variables can change
+weirdVar = 10
+p! typeof(weirdVar)
+
+puts "#{weirdVar} + #{weirdVar} = #{weirdVar + weirdVar}"
+
+weirdVar = "Hello, I changed!"
+p! typeof(weirdVar)
+puts weirdVar
+
+# Function to print out your Name
+def printName
+	puts "What is your first name?"
+	fName = gets
+	puts "What is your last name?"
+	lName = gets
+	
+	p! typeof(fName)
+	p! typeof(lName)
+	
+	puts
+	puts "Hello, #{fName} #{ lName}"
+	puts
+end
+
+counter = 0
+
+while counter < 2
+	printName
+	counter+=1
+end
